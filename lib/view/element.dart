b9348656127798e425a563c527e50bcc6effcfcb
王闯 <wangchuang@ymt360.com>
2020-01-20 14:36:39 +0800

    修改遍历结构

--- lib/view/element.dart
+++ lib/view/element.dart
@@ -15,77 +15,156 @@ class _Chemical extends State<Chemical> {
     print('Chemical');
   }
 
-  List<Widget> textList = [
-    Text(
-      '哈哈1',
-      style: TextStyle(color: Colors.black54),
-    ),
+  List<Map<String, dynamic>> textList = [
+    {
+      'title': '布局',
+      'color': 0xFFFFFFFF,
+      'descrtion': 'Layout',
+      'bgColor': 0xFF1cbbb4,
+      'bgImage': 'http://n.sinaimg.cn/sports/2_img/upload/cf0d0fdd/107/w1024h683/20181128/pKtl-hphsupx4744393.jpg',
+      'icon': 'format_textdirection_l_to_r'
+    },
+    {
+      'title': '背景',
+      'color': 0xFFFFFFFF,
+      'descrtion': 'Layout',
+      'bgColor': 0xFF0081ff,
+      'bgImage': 'http://n.sinaimg.cn/sports/2_img/upload/cf0d0fdd/107/w1024h683/20181128/pKtl-hphsupx4744393.jpg',
+      'icon': 'format_textdirection_l_to_r'
+    },
+    {
+      'title': '文本',
+      'color': 0xFFFFFFFF,
+      'descrtion': 'Layout',
+      'bgColor': 0xFF6739b6,
+      'bgImage': 'http://n.sinaimg.cn/sports/2_img/upload/cf0d0fdd/107/w1024h683/20181128/pKtl-hphsupx4744393.jpg',
+      'icon': 'format_textdirection_l_to_r'
+    },
+    {
+      'title': '图标',
+      'color': 0xFFFFFFFF,
+      'descrtion': 'Layout',
+      'bgColor': 0xFF9c26b0,
+      'bgImage': 'http://n.sinaimg.cn/sports/2_img/upload/cf0d0fdd/107/w1024h683/20181128/pKtl-hphsupx4744393.jpg',
+      'icon': 'format_textdirection_l_to_r'
+    },
+    {
+      'title': '按钮',
+      'color': 0xFFFFFFFF,
+      'descrtion': 'Layout',
+      'bgColor': 0xFFe03997,
+      'bgImage': 'http://n.sinaimg.cn/sports/2_img/upload/cf0d0fdd/107/w1024h683/20181128/pKtl-hphsupx4744393.jpg',
+      'icon': 'format_textdirection_l_to_r'
+    },
+    {
+      'title': '标签',
+      'color': 0xFFFFFFFF,
+      'descrtion': 'Layout',
+      'bgColor': 0xFFa5673f,
+      'bgImage': 'http://n.sinaimg.cn/sports/2_img/upload/cf0d0fdd/107/w1024h683/20181128/pKtl-hphsupx4744393.jpg',
+      'icon': 'format_textdirection_l_to_r'
+    },
+    {
+      'title': '头像',
+      'color': 0xFFFFFFFF,
+      'descrtion': 'Layout',
+      'bgColor': 0xFFe54d42,
+      'bgImage': 'http://n.sinaimg.cn/sports/2_img/upload/cf0d0fdd/107/w1024h683/20181128/pKtl-hphsupx4744393.jpg',
+      'icon': 'format_textdirection_l_to_r'
+    },
+    {
+      'title': '进度条',
+      'color': 0xFFFFFFFF,
+      'descrtion': 'Layout',
+      'bgColor': 0xFFf37b1d,
+      'bgImage': 'http://n.sinaimg.cn/sports/2_img/upload/cf0d0fdd/107/w1024h683/20181128/pKtl-hphsupx4744393.jpg',
+      'icon': 'format_textdirection_l_to_r'
+    },
+    {
+      'title': '边框阴影',
+      'color': 0xFFFFFFFF,
+      'descrtion': 'Layout',
+      'bgColor': 0xFF8dc63f,
+      'bgImage': 'http://n.sinaimg.cn/sports/2_img/upload/cf0d0fdd/107/w1024h683/20181128/pKtl-hphsupx4744393.jpg',
+      'icon': 'format_textdirection_l_to_r'
+    },
+    {
+      'title': '加载',
+      'color': 0xFFFFFFFF,
+      'descrtion': 'Layout',
+      'bgColor': 0xFF39b54a,
+      'bgImage': 'http://n.sinaimg.cn/sports/2_img/upload/cf0d0fdd/107/w1024h683/20181128/pKtl-hphsupx4744393.jpg',
+      'icon': 'format_textdirection_l_to_r'
+    },
 //    Image.network('http://n.sinaimg.cn/sports/2_img/upload/cf0d0fdd/107/w1024h683/20181128/pKtl-hphsupx4744393.jpg',
 //        fit: BoxFit.cover),
 //    Image.network('http://n.sinaimg.cn/sports/2_img/upload/cf0d0fdd/107/w1024h683/20181128/pKtl-hphsupx4744393.jpg',
 //        fit: BoxFit.cover),
-    Text('哈哈2'),
-    Text('哈哈3'),
 //    Image.network('http://i3.17173cdn.com/2fhnvk/YWxqaGBf/cms3/pTLITQbluyimehE.jpg!a-3-540x.jpg', fit: BoxFit.cover),
 //    Image.network('http://n.sinaimg.cn/sports/2_img/upload/cf0d0fdd/107/w1024h683/20181128/pKtl-hphsupx4744393.jpg',
 //        fit: BoxFit.cover),
-    Text('哈哈4'),
-    Text('哈哈5'),
 //    Image.network('http://i3.17173cdn.com/2fhnvk/YWxqaGBf/cms3/UaPFsMbluyimpCi.jpg!a-3-540x.jpg', fit: BoxFit.cover),
-    Text('哈哈6'),
 //    Image.network('http://i3.17173cdn.com/2fhnvk/YWxqaGBf/cms3/YyDzIGbluyimubl.jpg!a-3-540x.jpg', fit: BoxFit.cover),
-    Text('哈哈7'),
-    Text('哈哈8'),
-    Text('哈哈9'),
-    Text('哈哈10'),
-    Text('哈哈11'),
-    Text('哈哈12'),
-    Text('哈哈13'),
   ];
 
   List<Widget> viewList = [];
 
-  getListView() {
-    viewList = textList.map((item) {
-      Widget container = Container(
-          width: 130,
-          // 因为父盒子是 FractionallySizedBox 所以宽度不生效
+  Widget generateView(Map<String, dynamic> item) {
+    Widget container = Container(
+        width: 130,
+        // 因为父盒子是 FractionallySizedBox 所以宽度不生效
 //          height: 80,
-          margin: EdgeInsets.symmetric(vertical: 5, horizontal: 5),
-          // 用于设置对称方向的填充，vertical指top和bottom，horizontal指left和right。
-          padding: EdgeInsets.all(15),
-          decoration: BoxDecoration(
-              color: Colors.grey,
-              image: DecorationImage(
-                  image: NetworkImage('https://image.weilanwl.com/color2.0/cardBg.png'), fit: BoxFit.cover),
-              borderRadius: BorderRadius.all(Radius.circular(4.0))),
-          child: Column(
-            children: <Widget>[
-              Row(
+        margin: EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+        // 用于设置对称方向的填充，vertical指top和bottom，horizontal指left和right。
+        padding: EdgeInsets.all(15),
+        decoration: BoxDecoration(
+            color: Color(item['bgColor']),
+            image: DecorationImage(
+                image: NetworkImage('https://image.weilanwl.com/color2.0/cardBg.png'), fit: BoxFit.cover),
+            borderRadius: BorderRadius.all(Radius.circular(4.0))),
+        child: Column(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  item['title'],
+                  style: TextStyle(color: Color(item['color'])),
+                ),
+                Icon(
+                  Icons.format_textdirection_l_to_r,
+                  color: Colors.orange,
+                )
+              ],
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 10),
+              child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                textBaseline: TextBaseline.alphabetic,
+                // alphabetic 用于对齐字母字符的字形底部的水平线,相当于vertical-align: bottom; ideographic 用于对齐表意文字的水平线相当于vertical-align: middle
+
                 children: <Widget>[
-                  item,
-                  Icon(
-                    Icons.ac_unit,
-                    color: Colors.orange,
-                  )
+                  Text(
+                    item['descrtion'],
+                    style: TextStyle(color: Color(item['color'])),
+                  ),
+                  Text('23'),
                 ],
               ),
-              Container(
-                margin: EdgeInsets.only(top: 10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  textBaseline: TextBaseline.alphabetic,
-                  children: <Widget>[Text('Text'), Text('2')],
-                ),
-              )
-            ],
-          ));
-      return FractionallySizedBox(
-        // 设置盒子为父盒子的45%
-        widthFactor: 0.45,
-        child: container,
-      );
+            )
+          ],
+        ));
+    return FractionallySizedBox(
+      // 设置盒子为父盒子的45%
+      widthFactor: 0.45,
+      child: container,
+    );
+  }
+
+  getListView() {
+    viewList = textList.map((dynamic item) {
+      return generateView(item);
     }).toList();
   }
 
@@ -96,7 +175,6 @@ class _Chemical extends State<Chemical> {
     print('刘海屏状态栏高度:$statusBarHeight');
     print('屏幕宽度:$width');
     getListView();
-
     return Scaffold(
       backgroundColor: Colors.transparent,
 //      appBar: AppBar(
@@ -110,14 +188,14 @@ class _Chemical extends State<Chemical> {
 //          padding: EdgeInsets.only(top: statusBarHeight), // 设置padding
 //        margin: EdgeInsets.only(left: width), // 设置margin
           decoration: BoxDecoration(
-              color: Colors.red,
+//              color: Colors.red,
 //            border: Border.all(width: 2.0, color: Colors.grey), // 设置边框
 //            borderRadius: BorderRadius.all(Radius.circular(4.0)), // 设置圆角
               image: DecorationImage(
-                image: NetworkImage('https://up.enterdesk.com/edpic/f8/e6/97/f8e69731db3d3ab863c877c79e7f9eea.jpg'),
-                fit: BoxFit.cover,
-                alignment: Alignment.topCenter,
-              )),
+            image: NetworkImage('https://up.enterdesk.com/edpic/f8/e6/97/f8e69731db3d3ab863c877c79e7f9eea.jpg'),
+            fit: BoxFit.cover,
+            alignment: Alignment.topCenter,
+          )),
           alignment: Alignment.topCenter,
           child: SingleChildScrollView(
             child: Column(
